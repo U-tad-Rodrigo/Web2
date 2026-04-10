58e65bce39044ab082a4b3c7c4e5036e5b2dd714
Rodrigo Fernandez <rodrixfc17@gmail.com>
2026-04-11 01:36:29 +0200

    T10 updates

--- ejercicios/T10/src/models/user.model.js
+++ ejercicios/T10/src/models/user.model.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true, trim: true, minlength: 3, maxlength: 30 },
     email: { type: String, required: true, unique: true, trim: true, lowercase: true },
-    password: { type: String, required: true },
+    password: { type: String, required: true, minlength: 6 },
   },
   { timestamps: true }
 );
